ad43c15ceb2723b5193d2156d3decd34adf4e209
Alissa Reynolds <alissa6710@gmail.com>
2023-12-07 12:14:39 -0600

    finshed adding basic layout for html pages and finished javascript files

--- src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
+++ src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
@@ -23,20 +23,20 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public List<String> addUser(UserDto userDto) {
+        List<String> response = new ArrayList<>();
         User user = new User(userDto);
         userRepository.saveAndFlush(user);
-        response.add("User Added Seccussfully");
+        response.add("http://localhost:8080/login.html");
         return response;
     }
 
     @Override
-    @Transactional
     public List<String> userLogin(UserDto userDto) {
         List<String> response = new ArrayList<>();
         Optional<User> userOptional = userRepository.findByUsername(userDto.getUsername());
         if (userOptional.isPresent()) {
             if (passwordEncoder.matches(userDto.getPassword(), userOptional.get().getPassword())) {
-                response.add("User Login Successful");
+                response.add("http://localhost:8080/home.html");
                 response.add(String.valueOf(userOptional.get().getId()));
             } else {
                 response.add("Username or password incorrect");
